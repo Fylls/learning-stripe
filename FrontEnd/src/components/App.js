70073bc793e6b253d74a5249877a81a2097858f0
Fylls <55017307+Fylls@users.noreply.github.com>
2020-07-22 14:54:04 +0200

    Update App.js

--- FrontEnd/src/components/App.js
+++ FrontEnd/src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react"
+
 // Components
 import HomePage from "./HomePage"
+
 // Stripe
 import { Elements } from "@stripe/react-stripe-js"
 import { loadStripe } from "@stripe/stripe-js"
+
 // Styles
 import "../index.scss"
 
-const stripePromise = loadStripe(
-  "pk_test_51Gs0xOBhiUsS8BKcBBqQlYVYsR3wc7y6Levt6aazlyuMkzkqh5tLhNud4nS1wJEjJUawbfvDSYndtrQWTfnd3W5q00qB2Fhv41"
-)
+const stripePromise = loadStripe(" *** ")
 
 function App() {
   return (
